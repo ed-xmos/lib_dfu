158fbc5100e3c9e2980278962cd74e40bb92680f
Larry Snizek <larry@xmos.com>
2019-12-20 20:16:57 +0000

    Convert host tests into Pytest

---
+++ tests/host/data_partition_generator/crc_algorithm/test_crc_algorithm.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2019, XMOS Ltd, All rights reserved
+import subprocess, os
+
+def test_crc_algorithm():
+    home = os.path.dirname(os.path.abspath(__file__))
+    try:
+        cmd = [os.path.join(home, 'crc_algorithm')]
+        output = subprocess.check_call(cmd)
+    except subprocess.CalledProcessError as e:
+        msg = '''Error! Test failed
+               \ncmd: %s
+               \noutput: %s
+               \nreturn_code: %d'''\
+               % (str(e.cmd), e.output, e.returncode)
+        raise Exception(msg)
+
+if __name__ == "__main__":
+    print('test_crc_algorithm')
+    test_crc_algorithm()
