9c36f77e3d67f42cf866ec2506062ce7ae373b97
Larry Snizek <larry@xmos.com>
2020-01-06 15:11:20 +0000

    Boot flash util module with tests

---
+++ tests/device_simulation/boot_flash_util/nibble_swap/nibble_swap.c
@@ -0,0 +1,25 @@
+// Copyright (c) 2019, XMOS Ltd, All rights reserved
+#include <xs1.h>
+#include <print.h>
+
+#define XASSERT_ENABLE_DEBUG 1
+#define XASSERT_ENABLE_LINE_NUMBERS 1
+#include "xassert.h"
+
+#include "boot_flash_util.h"
+
+unsigned DeviceAccess_ReadWord(unsigned word_address)
+{
+  assert(0);
+  return -1;
+}
+
+unsigned DEVICE_SECTOR_WORD_SIZE = 1024;
+
+int main(void)
+{
+  assert(nibble_swap_byte(0x12) == 0x21);
+  assert(nibble_swap_word(0xABCD1234) == 0xBADC2143);
+  printstr("PASS\n");
+  return 0;
+}
---
+++ tests/device_simulation/boot_flash_util/sector_at_or_after/sector_at_or_after.c
@@ -0,0 +1,35 @@
+// Copyright (c) 2019, XMOS Ltd, All rights reserved
+#include <xs1.h>
+#include <print.h>
+
+#define XASSERT_ENABLE_DEBUG 1
+#define XASSERT_ENABLE_LINE_NUMBERS 1
+#include "xassert.h"
+
+#include "boot_flash_util.h"
+
+unsigned DeviceAccess_ReadWord(unsigned word_address)
+{
+  assert(0);
+  return -1;
+}
+
+unsigned DEVICE_SECTOR_WORD_SIZE = 1024;
+
+int main(void)
+{
+  assert(sector_address_at_or_after(0) == 0);
+
+  assert(sector_address_at_or_after(1) == 4096);
+  assert(sector_address_at_or_after(1024) == 4096);
+  assert(sector_address_at_or_after(4095) == 4096);
+  assert(sector_address_at_or_after(4096) == 4096);
+
+  assert(sector_address_at_or_after(4097) == 8192);
+  assert(sector_address_at_or_after(5120) == 8192);
+  assert(sector_address_at_or_after(8191) == 8192);
+  assert(sector_address_at_or_after(8192) == 8192);
+
+  printstr("PASS\n");
+  return 0;
+}
