2bb176ac8cf53258d81873874318cc7ea3b56da9
Larry Snizek <larry@xmos.com>
2020-01-07 10:56:55 +0000

    Library checks

--- tests/boot_hardware/helpers/cafecafe_60000.c
+++ tests/boot_hardware/helpers/cafecafe_60000.c
@@ -1,3 +1,4 @@
+// Copyright (c) 2020, XMOS Ltd, All rights reserved
 int main(void)
 {
   *((unsigned*)0x60000) = 0xCAFECAFE;
--- tests/device_simulation/quadflash_data_partition/crc_algorithm/test_c.c
+++ tests/device_simulation/quadflash_data_partition/crc_algorithm/test_c.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, XMOS Ltd, All rights reserved
+// Copyright (c) 2019-2020, XMOS Ltd, All rights reserved
 #include <assert.h>
 #include "quadflash_crc.h"
 
