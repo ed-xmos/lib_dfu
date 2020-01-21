ca9f8ae06b21fff2d84613557113ad1a2fee8a13
Larry Snizek <larry@xmos.com>
2020-01-21 16:36:17 +0000

    Make main header host friendly

--- lib_dfu/api/dfu.h
+++ lib_dfu/api/dfu.h
@@ -3,8 +3,10 @@
 #define __dfu_h__
 
 #include <stddef.h>
+#ifdef __xcore__
 #include <xccompat.h>
 #include <quadflash.h>
+#endif
 
 #define DFU_BLOCK_SIZE_MAX_BYTES 512
 #define DFU_PAGE_SIZE_MAX_BYTES 256
@@ -48,6 +50,7 @@ struct dfu_getstatus {
   unsigned poll_timeout_msec;
 };
 
+#ifdef __xcore__
 enum dfu_state dfu_getstate(void);
 
 struct dfu_getstatus dfu_getstatus(void);
@@ -63,5 +66,6 @@ void dfu_timeout_detach(void);
 
 void dfu_dnload(unsigned short block_num, size_t block_size_bytes,
                 const char block[DFU_BLOCK_SIZE_MAX_BYTES]);
+#endif
 
 #endif
