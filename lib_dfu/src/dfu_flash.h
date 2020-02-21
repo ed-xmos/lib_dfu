58343c7c637f90bc572d92d251947a8d1b323b04
Larry Snizek <larry@xmos.com>
2020-02-21 17:00:14 +0000

    Documentation

--- lib_dfu/src/dfu_flash.h
+++ lib_dfu/src/dfu_flash.h
@@ -1,4 +1,8 @@
 // Copyright (c) 2019-2020, XMOS Ltd, All rights reserved
+//
+// The original intention is for those functions to be the ones used by the
+// DFU implementation, and those only
+//
 #ifndef __dfu_flash_h__
 #define __dfu_flash_h__
 
@@ -7,11 +11,9 @@
 #define _Bool int
 #include <stdbool.h>
 
-int flash_locate_boot_upgrade_slot(REFERENCE_PARAM(unsigned, address));
-
-int flash_locate_data_upgrade_slot(REFERENCE_PARAM(unsigned, address));
-
 int flash_erase_sector_async(unsigned address);
+
+int flash_get_page_size(void);
 
 bool flash_is_busy(void);
 
@@ -19,12 +21,14 @@ bool flash_is_first_whole_page_in_sector(unsigned address);
 
 bool flash_is_sector_erased(unsigned address);
 
-int flash_set_write_disable(void);
+int flash_locate_boot_upgrade_slot(REFERENCE_PARAM(unsigned, address));
 
-int flash_write_page_async(unsigned address, const char page[]);
+int flash_locate_data_upgrade_slot(REFERENCE_PARAM(unsigned, address));
+
+int flash_set_write_disable(void);
 
 int flash_verify_page(unsigned address, const char page[]);
 
-int flash_get_page_size(void);
+int flash_write_page_async(unsigned address, const char page[]);
 
 #endif
