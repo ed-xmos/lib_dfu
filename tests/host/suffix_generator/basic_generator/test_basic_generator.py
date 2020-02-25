99d495d8b84ed69424778548e922f7d50fdf15aa
lucianom <lucianom@xmos.com>
2020-02-25 15:47:54 +0000

    Update tests to use cmake

--- tests/host/suffix_generator/basic_generator/test_basic_generator.py
+++ tests/host/suffix_generator/basic_generator/test_basic_generator.py
@@ -1,26 +1,27 @@
 # Copyright (c) 2020, XMOS Ltd, All rights reserved
 import subprocess, os
+import filecmp
+from pathlib import Path
+
+HOME = Path(__file__).resolve().parent
 
 def test_basic_generator():
-    home = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(home, 'input.bin'), 'rb') as in_file:
-        cmd = [os.path.join(home, 'bin', 'suffix_generator'),
-               '0x20B1', '0x0014']
-        try:
-            output = subprocess.check_output(cmd, stdin=in_file)
-        except subprocess.CalledProcessError as e:
-            msg = '''Error! Test failed
-                   \ncmd: %s
-                   \nreturn_code: %d'''\
-                   % (' '.join(e.cmd), e.returncode)
-            raise Exception(msg)
-        with open(os.path.join(home, 'golden.bin'), 'rb') as golden_file:
-            expected = golden_file.read()
-            if output != expected:
-                raise Exception('''Error! Test failed
-                      \ncommand line: %s'''\
-                      % ' '.join(cmd))
-        print('PASS')
+    os.chdir(HOME)
+    cmd = ['../../../../host/suffix_generator/bin/dfu_suffix_generator',
+           '0x20B1', '0x0014', 'input.bin', 'output.bin' ]
+    try:
+        output = subprocess.check_output(cmd)
+    except subprocess.CalledProcessError as e:
+        msg = '''Error! Test failed
+                \ncmd: %s
+                \nreturn_code: %d'''\
+                % (' '.join(e.cmd), e.returncode)
+        raise Exception(msg)
+    if not filecmp.cmp(HOME / 'output.bin', HOME / 'golden.bin' ):
+        raise Exception('''Error! Test failed
+              \ncommand line: %s'''\
+              % ' '.join(cmd))
+    print('PASS')
 
 if __name__ == "__main__":
     print('test_basic_generator')
